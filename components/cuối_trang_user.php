5947945dd0c4ce11af8ee4edbd34875397d2acc1
longza3 <167165532+longza3@users.noreply.github.com>
2024-04-16 17:44:05 +0700

    Add files via upload

---
+++ components/cuối_trang_user.php
@@ -0,0 +1,52 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>đường dẫn </h3>
+         <a href="trang_chủ.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-house"></i>trang chủ</a>
+         <a href="giới_thiệu.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-circle-info"></i>about</a>
+         <a href="trang_chủ.php?#cuahang"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-shop"></i>cửa hàng</a>
+         <a href="liên_hệ.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-address-book"></i>liên hệ</a>
+      </div>
+
+      <div class="box">
+         <h3>Liên kết phụ</h3>
+         <a href="login.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-right-to-bracket"></i>đăng nhập</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i><i class="fa-regular fa-registered"></i>đăng ký</a>
+         <a href="giỏ_hàng.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-cart-shopping"></i>giỏ hàng </a>
+         <a href="đơn_hàng.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-truck-fast"></i>đơn hàng</a>
+      </div>
+
+      <div class="box">
+         <h3>Liên hệ chúng tôi</h3>
+         <a href="#" click="clickToCopy('0335196106')"><i class="fas fa-phone"></i> 0333333333</a>
+         <a href="#"><i class="fas fa-phone"></i> 03333333333</a>
+         <a href="#"><i class="fas fa-envelope"></i> computermark@gmail.com</a>
+         <a href="https://www.google.com/maps"><i class="fas fa-map-marker-alt"></i> Hà Nội </a>
+      </div>
+
+      <div class="box">
+         <h3>theo dõi chúng tôi</h3>
+         <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i>youtobe</a>
+      </div>
+
+   </section>
+   <style>
+      .fa-star{
+         color: #f39c12;
+      }
+   </style>
+   <div class="credit"><i class="fa-solid fa-barcode"></i> @ <?= date('Y'); ?> <i class="fa-solid fa-barcode"></i>  <span>Nhóm 10</span> | 
+       <i class="fa-solid fa-star" ></i>
+       <i class="fa-solid fa-star" ></i>
+       <i class="fa-solid fa-star" ></i>
+       <i class="fa-solid fa-star" ></i>
+       <i class="fa-solid fa-star" ></i>
+   </div>
+   
+
+</footer>
